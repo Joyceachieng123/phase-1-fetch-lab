a375636cf30f2e0e45fbd5a0f86b33265e411e7d
Joyce Achieng <joyce.achieng@student.moringaschool.com>
2023-09-29 08:31:07 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  const url = 'https://anapioficeandfire.com/api/books';
+  return fetch(url)
+    
+  .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      renderBooks(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   
 }
 
